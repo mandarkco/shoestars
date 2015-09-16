8bed5a540c670c1fecdb9c9c0fa26ae91aa8c8a8
jhoncbuendia <jhoncbuendia@gmail.com>
2015-09-16 17:49:32 -0500

    Quitar numeros en informacion y autentacion al momento de comprár un producto
    Revisar tildes
    
    Titulo de bienvenida y nombre en negro y mas pequeño
    
    Quitar flechas y puntos de el slider
    
    Fixear modo lista

--- shoe_starts/settings.py
+++ shoe_starts/settings.py
@@ -115,8 +115,8 @@ CSRF_COOKIE_SECURE = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATIC_URL = '/static/'
-STATIC_URL = 'http://54.200.219.177/shoe_starts/static/'
+STATIC_URL = '/static/'
+#STATIC_URL = 'http://54.200.219.177/shoe_starts/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
